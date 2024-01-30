92aed6bba4aed0178f63d998bf5cf26a5e1804a6
JuanCarlosRossLopez <berpyboy10@gmail.com>
2024-01-30 11:19:22 -0500

    Cambios ross

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import Navbar from "./components/Navbar";
+import "boxicons";
+import Card from "./components/Card";
+import imagen from "./assets/istockphoto-1478431022-1024x1024.jpg";
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex flex-col ">
+      <div className="">
+        <Navbar />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count +10)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className=" mt-16">
+        <Card image={imagen} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Card = ({image}) => {
+  return (
+    <div className="min-h-screen ">
+      <div className=" bg-white py-10">
+        <div className="py-10 text-center">
+          <div className="font-extrabold text-4xl text-black ">
+            BIENVENIDO A UT-CANICOS <br />
+            JUAN ROSS
+          </div>
+        </div>
+        <div className=" flex items-center justify-center  ">
+          <img
+            className="h-96 rounded-2xl"
+            src={image}
+            alt="Imagen de empresa"
+          />
+        </div>
+        <div className="px-6 pt-10 text-center">
+          <h2 className="font- text-base text-black pb-10">
+            Es un hecho establecido hace demasiado tiempo que un lector se
+            distraerá con el contenido del texto de un sitio mientras que mira
+            su diseño. El punto de usar Lorem Ipsum es que tiene una
+            distribución más o menos normal de las letras, al contrario de usar
+            textos como por ejemplo "Contenido aquí, contenido aquí". Estos
+            textos hacen parecerlo un español que se puede leer. Muchos paquetes
+            de autoedición y editores de páginas web usan el Lorem Ipsum como su
+            texto por defecto, y al hacer una búsqueda de "Lorem Ipsum" va a dar
+            por resultado muchos sitios web que usan este texto si se encuentran
+            en estado de desarrollo. Muchas versiones han evolucionado a través
+            de los años, algunas veces por accidente, otras veces a propósito
+            (por ejemplo insertándole humor y cosas por el estilo).
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
