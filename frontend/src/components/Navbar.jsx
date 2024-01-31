43e83498677ffb5b13279d2c411c6d88ff89ae7c
OsitoDeArroz <yahir15000@hotmail.com>
2024-01-30 23:14:34 -0500

    se agrego el login y registro

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,23 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
     <div className="bg-red-700 fixed top-0 w-full h-24 gap-16 flex items-center justify-center ">
-      <div className="text-white text-xl font-extrabold  ml-20">
+      <div className="text-white text-xl font-extrabold h-30 ml-20">
         <h1>Ut-canicos</h1>
       </div>
-      <div className="text-3xl text-white font-extrabold ps-36 pe-96 pt-7">
+      <div className="text-5xl text-white font-extrabold ps-36 pe-96 pt-7">
         <ul>
-          <li className="hover:text-black">Empezar</li>
+          <li>Empezar</li>
         </ul>
       </div>
-      <div className="flex flex-col items-center mr-32">
-        <box-icon name="user-circle" size="lg" color="#ffffff"></box-icon>
-        <a
-          href="/"
-          className="text-white hover:text-black font-extrabold text-xs"
-        >
-          Perfil
-        </a>
+      <div className="flex flex-col items-center h-8 mr-32">
+       <Link to={"/login"}>
+       <box-icon name="user-circle" size="lg" color="#ffffff"></box-icon>
+       </Link>
       </div>
     </div>
   );
