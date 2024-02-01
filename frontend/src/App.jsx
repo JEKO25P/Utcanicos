1f7ee891a3aad3a4e72bd229b22a00bc70823599
JuanCarlosRossLopez <berpyboy10@gmail.com>
2024-02-01 00:38:46 -0500

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import Navbar from "./components/Navbar";
 import "boxicons";
 import Card from "./components/Card";
-import imagen from "./assets/istockphoto-1478431022-1024x1024.jpg";
+import imagen from "./assets/auto.jpg";
 export default function App() {
   return (
     <div className="flex flex-col">
